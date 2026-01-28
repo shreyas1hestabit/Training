38541bce60873c872a75f651c41a8ffa383af98c
Shreya Singhal <shreyas1.hestabit@gmail.com>
2026-01-28 19:53:13 +0530

    week4 day1 only step1 done

---
+++ Week4/test.js
@@ -0,0 +1,3 @@
+//yeh hamre port ko test krne k liye hai. basically hmara code chl bh rha hai ya nh woh chekc krne k liye ek temporary js file.
+import { config } from "./src/config/index.js"; //hm yha woh file with its config import krate hai jiski working hme check krni hai.
+console.log(config);
